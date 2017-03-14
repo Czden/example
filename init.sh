7918251eeadc2cb38b8edd9fb51009f4deb41366
Den <czden_@mail.ru>
2017-03-14 16:50:10 +1000

    Изменения для закрепления: new file:   init.sh create forder tree for web server

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir /home/box/web
+mkdir /home/box/web/public
+mkdir /home/box/web/public/img
+mkdir /home/box/web/public/css
+mkdir /home/box/web/public/js
+mkdir /home/box/web/uploads
+mkdir /home/box/web/etc
+
