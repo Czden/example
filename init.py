d77cdbf74f82bd432b9e70dd7ed17eb91ce002ed
Denis <czden_@mail.ru>
2017-03-17 23:53:51 +1000

    вариант 1 от 17.03.2017

--- init.py
+++
@@ -1,6 +0,0 @@
-import socket
-sock=socket.socket('....')
-sock.bind(...)
-sock.listen(5)
-while True:
-    conn,addr=sock.accept()
--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
-mkdir /home/box/web
-mkdir /home/box/web/public
-mkdir /home/box/web/public/img
-mkdir /home/box/web/public/css
-mkdir /home/box/web/public/js
-mkdir /home/box/web/uploads
-mkdir /home/box/web/etc
-cp /home/box/web/nginx.conf /home/box/web/etc/nginx.conf 
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enables/test.conf
+mkdir home/box/web/public
+mkdir home/box/web/public/img
+mkdir home/box/web/public/css
+mkdir home/box/web/public/js
+mkdir home/box/web/uploads
+mkdir home/box/web/etc
+cp home/box/web/src/nginx.conf home/box/web/etc/nginx.conf 
+sudo ln -s home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
