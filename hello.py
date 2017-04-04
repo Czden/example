e8172b6dd3bdbc13de9774833b3d7dc6bf5066de
Czden <czden_@mail.ru>
2017-04-04 17:15:17 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
+import urlib
 def my_app(env, start_resp):
     outer=list()
     status='200 OK'
     resp_header=[('Content-type','text/plain')]
     try:
-        params=list(env['QUERY_STRING'].split('&'))
-        for par in params:
-            outer.append(par)
+        params=urllib.parse.parse_qs(env['QUERY_STRING'])
+        for key in params:
+            outer.append('{0}={1}'.format(key, params.get(key)[-1]))
             outer.append('\r\n')
     except KeyError: pass
     start_resp(status, resp_header)
